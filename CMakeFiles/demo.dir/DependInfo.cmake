dad158ffd797b8f50b67f5b63e8187ed92012111
caizh <caizh@LAPTOP-7SBDM3HO.localdomain>
2022-12-11 22:48:29 +0800

    test

--- CMakeFiles/demo.dir/DependInfo.cmake
+++ CMakeFiles/demo.dir/DependInfo.cmake
@@ -1,18 +1,20 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
   )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/caizh/CPlusLearning_Caizh/Source/LeetCodeSolution.cpp" "/home/caizh/CPlusLearning_Caizh/CMakeFiles/demo.dir/Source/LeetCodeSolution.cpp.o"
+  "/home/caizh/CPlusLearning_Caizh/Source/main.cpp" "/home/caizh/CPlusLearning_Caizh/CMakeFiles/demo.dir/Source/main.cpp.o"
+  "/home/caizh/CPlusLearning_Caizh/Source/mathFcn.cpp" "/home/caizh/CPlusLearning_Caizh/CMakeFiles/demo.dir/Source/mathFcn.cpp.o"
+  "/home/caizh/CPlusLearning_Caizh/Source/nQueen.cpp" "/home/caizh/CPlusLearning_Caizh/CMakeFiles/demo.dir/Source/nQueen.cpp.o"
+  "/home/caizh/CPlusLearning_Caizh/Source/testFcns.cpp" "/home/caizh/CPlusLearning_Caizh/CMakeFiles/demo.dir/Source/testFcns.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/mnt/d/Programs/CPlusLearning_Caizh/Source/LeetCodeSolution.cpp" "CMakeFiles/demo.dir/Source/LeetCodeSolution.cpp.o" "gcc" "CMakeFiles/demo.dir/Source/LeetCodeSolution.cpp.o.d"
-  "/mnt/d/Programs/CPlusLearning_Caizh/Source/main.cpp" "CMakeFiles/demo.dir/Source/main.cpp.o" "gcc" "CMakeFiles/demo.dir/Source/main.cpp.o.d"
-  "/mnt/d/Programs/CPlusLearning_Caizh/Source/mathFcn.cpp" "CMakeFiles/demo.dir/Source/mathFcn.cpp.o" "gcc" "CMakeFiles/demo.dir/Source/mathFcn.cpp.o.d"
-  "/mnt/d/Programs/CPlusLearning_Caizh/Source/nQueen.cpp" "CMakeFiles/demo.dir/Source/nQueen.cpp.o" "gcc" "CMakeFiles/demo.dir/Source/nQueen.cpp.o.d"
-  "/mnt/d/Programs/CPlusLearning_Caizh/Source/testFcns.cpp" "CMakeFiles/demo.dir/Source/testFcns.cpp.o" "gcc" "CMakeFiles/demo.dir/Source/testFcns.cpp.o.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "Include"
   )
 
 # Targets to which this target links.
--- CMakeFiles/demo.dir/cmake_clean.cmake
+++ CMakeFiles/demo.dir/cmake_clean.cmake
@@ -1,14 +1,9 @@
 file(REMOVE_RECURSE
   "CMakeFiles/demo.dir/Source/LeetCodeSolution.cpp.o"
-  "CMakeFiles/demo.dir/Source/LeetCodeSolution.cpp.o.d"
   "CMakeFiles/demo.dir/Source/main.cpp.o"
-  "CMakeFiles/demo.dir/Source/main.cpp.o.d"
   "CMakeFiles/demo.dir/Source/mathFcn.cpp.o"
-  "CMakeFiles/demo.dir/Source/mathFcn.cpp.o.d"
   "CMakeFiles/demo.dir/Source/nQueen.cpp.o"
-  "CMakeFiles/demo.dir/Source/nQueen.cpp.o.d"
   "CMakeFiles/demo.dir/Source/testFcns.cpp.o"
-  "CMakeFiles/demo.dir/Source/testFcns.cpp.o.d"
   "demo"
   "demo.pdb"
 )
